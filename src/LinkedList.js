4fd4ba4027f9359a2cab69a93354047f9ebd5a92
PanahovKh <xanali.panahov1994@gmail.com>
2020-03-10 19:50:40 +0400

    Add Tests

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,4 +1,8 @@
-const LinkedNode = require('./LinkedNode');
+
+function LinkedNode(value) {
+    this.value = value;
+    this.next = null;
+}
 
 function LinkedList () {
     this.root = null;
@@ -6,68 +10,67 @@ function LinkedList () {
 }
 
 LinkedList.prototype.addElmAtStart = function (value) {
+    let tempNode = new LinkedNode(value);
 
-    let tempNode  = new LinkedNode (value) 
-    
     tempNode.next = this.root;
     this.root = tempNode;
     this.size += 1;
-}
+
+    return this.size;
+};
 
 LinkedList.prototype.init = function (array) {
-    for(let i = 0; i < array.length; i++) {
-         this.addElmAtStart (array[i]) 
+    if (!Array.isArray(array)) {
+        return 'Please check your input!'
     }
-}
+    for (let i = array.length - 1; i >= 0; i--) {
+        this.addElmAtStart(array[i]);
+    }
+};
 
 LinkedList.prototype.tooArray = function(value) {
-    
     let arr = [];
     let tempNode = this.root;
+
     while(tempNode) {
-        arr.push (tempNode.value) 
+        arr.push(tempNode.value)
         tempNode = tempNode.next
-    }
+    };
+
     return arr;
-}
+};
 
 LinkedList.prototype.clear = function() { 
-
     this.root = null;
     this.size = 0;
-}
+};
 
 LinkedList.prototype.toString = function() {
     let arrToString = '"';
     let tempNode = this.root;
+
     while(tempNode) {
         arrToString += tempNode.value + ', ';
         tempNode = tempNode.next
     }
-    arrToString += '"'
-    return arrToString;
-}
+    arrToString += '"';
 
-LinkedList.prototype.getSize = function() {
-    let result = 0;
-    tempNode = this.root
-    while(tempNode) { 
-        tempNode = tempNode.next;
-        
-        result ++; 
-    }
-    return result
-}
+    return arrToString;
+};
+
+LinkedList.prototype.getSizeOfList = function() {
+    return this.size;
+};
 
 LinkedList.prototype.delFirstElm = function() {
     let firstElm = this.root.value;
     let tempNode = this.root.next;
 
     this.root = tempNode;
-    this.size--;
+    this.size -= 1;
 
     return firstElm;
-}
+};
 
 LinkedList.prototype.delLastElm = function() {
     let result = null; 
@@ -77,38 +80,33 @@ LinkedList.prototype.delLastElm = function() {
     while(tempNode) { 
         previous = tempNode; 
         tempNode = tempNode.next;
+
         if(!tempNode.next) {
             result = tempNode.value;
             previous.next = null;
             this.size -= 1;
+
             return result;
-        }
-    }
-}
+        };
+    };
+};
 
 LinkedList.prototype.addElmToEnd = function(value) {
     let tempNode = this.root;
     let newNode = new LinkedNode(value);
+
     while(tempNode){
         tempNode = tempNode.next;
         if(!tempNode.next) {
             tempNode.next = newNode;
-            this.size += 1
-            return this.tooArray();
-        }
-    }
-}
+            this.size += 1;
+
+            return this.size;
+        };
+    };
+};
 
 const a  = new LinkedList();
-a.init ([1 ,3 ,5, 6]);
-// console.log(a.tooArray());
-// console.log(a.toString());
- console.log(a.getSize());
-// console.log(a.delFirstElm());
-// console.log(a.delLastElm());
-// console.log(a);
 
-// console.log(a.addElmToEnd(9));
-// console.log(a);
 
 
